86be8b6b4ad417af385c3fe752d670198ffd931c
laidet_r <rodolphe.laidet@epitech.eu>
2016-05-23 17:51:53 +0200

    Création des fichiers controllers et services, qui vont servir à acceuillir les controllers et les services.... ^^' Créations dedans des modules qui vont bien

---
+++ my_snapchat/www/js/controllers.js
@@ -0,0 +1,8 @@
+/*jslint browser this for */
+/*global angular alert */
+
+(function () {
+    'use strict';
+
+    var controllers = angular.module('my_snapchat.controllers', []);
+}());
---
+++ my_snapchat/www/js/services.js
@@ -0,0 +1,8 @@
+/*jslint browser this for */
+/*global angular alert */
+
+(function () {
+    'use strict';
+
+    var services = angular.module('my_snapchat.services', []);
+}());
