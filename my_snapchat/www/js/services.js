99fac47041a1c21a4a37de23f72f33a58450419d
laidet_r <rodolphe.laidet@epitech.eu>
2016-05-25 21:20:44 +0200

    Creation d'un service pour gérer les users methodes User, genre register, login, logout etc...

--- my_snapchat/www/js/services.js
+++ my_snapchat/www/js/services.js
@@ -5,4 +5,15 @@
     'use strict';
 
     var services = angular.module('my_snapchat.services', []);
+
+    services.service('UserService', function ($http) {
+        var self = this;
+        this.apiUrl = 'http://snapchat.samsung-campus.net/api.php?option=inscription';
+
+        this.register = function (user, successCallback) {
+            $http.post(self.apiUrl, user)
+                .then(successCallback);
+        };
+
+    });
 }());
