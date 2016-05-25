97620be8870c017a13ff2ef04d0da23c1762e2e9
laidet_r <rodolphe.laidet@epitech.eu>
2016-05-25 22:26:30 +0200

    Passage de la base de l'url de l'API en variable, le reste en propre aux methode. Rajout des methodes login et tout ce qui est localStorage des credentilals au UserService

--- my_snapchat/www/js/services.js
+++ my_snapchat/www/js/services.js
@@ -8,12 +8,29 @@
 
     services.service('UserService', function ($http) {
         var self = this;
-        this.apiUrl = 'http://snapchat.samsung-campus.net/api.php?option=inscription';
+        this.apiUrl = 'http://snapchat.samsung-campus.net/api.php';
 
         this.register = function (user, successCallback) {
-            $http.post(self.apiUrl, user)
+            $http.post(self.apiUrl + '?option=inscription', user)
                 .then(successCallback);
         };
 
+        this.login = function (user, successCallback) {
+            $http.post(self.apiUrl + '?option=connexion', user)
+                .then(successCallback);
+        };
+
+        this.storeCredentials = function (credentials) {
+            localStorage.setItem('My_Snapchat_Credentials', JSON.stringify(credentials));
+            return true;
+        };
+
+        this.loadCredentials = function () {
+            return localStorage.getItem('My_Snapchat_Credentials') || false;
+        };
+
+        this.clearCredentials = function () {
+            localStorage.removeItem('My_Snapchat_Credentials');
+        }
     });
 }());
