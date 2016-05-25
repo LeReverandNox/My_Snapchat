91f13b27ef0a90f0beec01bd419ce04f09e7d1d3
laidet_r <rodolphe.laidet@epitech.eu>
2016-05-25 21:19:50 +0200

    Remplissage du controller RegisterCtrl pour gerer l'incription, avec popups et redirections

--- my_snapchat/www/js/controllers.js
+++ my_snapchat/www/js/controllers.js
@@ -7,9 +7,32 @@
     var controllers = angular.module('my_snapchat.controllers', []);
 
     controllers.controller('AppCtrl', function () {
+        console.log("App");
     });
     controllers.controller('IndexCtrl', function ($scope) {
+        console.log("Index");
     });
-    controllers.controller('RegisterCtrl', function ($scope, $http) {
+    controllers.controller('RegisterCtrl', function ($scope, UserService, $ionicPopup, $location) {
+        $scope.user = {};
+
+        $scope.register = function (user) {
+            UserService.register(user, function (response) {
+                if (response.data.error === true) {
+                    $ionicPopup.alert({
+                        title: 'Hurray !',
+                        template: 'You\'re now registered on My_Snapchat !'
+                    }).then(function () {
+                        $location.path('/');
+                    });
+                } else {
+                    $ionicPopup.alert({
+                        title: 'Error',
+                        template: response.data.error
+                    }).then(function () {
+                        $scope.user = {};
+                    });
+                }
+            });
+        };
     });
 }());
