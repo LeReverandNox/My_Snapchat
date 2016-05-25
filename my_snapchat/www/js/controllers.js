fa1fa761a4c5b2ebb701d52417988ed55327f913
laidet_r <rodolphe.laidet@epitech.eu>
2016-05-25 12:03:15 +0200

    Création des controllers vide pour App, Index et Register

--- my_snapchat/www/js/controllers.js
+++ my_snapchat/www/js/controllers.js
@@ -5,4 +5,11 @@
     'use strict';
 
     var controllers = angular.module('my_snapchat.controllers', []);
+
+    controllers.controller('AppCtrl', function () {
+    });
+    controllers.controller('IndexCtrl', function ($scope) {
+    });
+    controllers.controller('RegisterCtrl', function ($scope, $http) {
+    });
 }());
