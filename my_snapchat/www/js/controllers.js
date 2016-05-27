1860f9d9092d76140c405397e29b2c353abb9029
laidet_r <rodolphe.laidet@epitech.eu>
2016-05-27 10:58:24 +0200

    Ajout du controller SendSnap avec toutes les methodes qui vont bien pour envoyer un snap,, avec quelques techiques de gitan au millieux. Ca marche !

--- my_snapchat/www/js/controllers.js
+++ my_snapchat/www/js/controllers.js
@@ -82,4 +82,89 @@
 
     controllers.controller('OptionsCtrl', function () {
     });
+
+    controllers.controller('SendSnapCtrl', function ($scope, SnapService, ToolsService, UserService, $ionicPopup) {
+        console.log("SnapCtrl");
+        $scope.isSnaping = false;
+        $scope.isGoingToChooseUsers = false;
+        $scope.isChoosingUsers = false;
+        $scope.isSelectingTime = false;
+        var snapHolder = document.querySelector('.send-snap-holder');
+
+        this.image = null;
+        this.credentials = UserService.loadCredentials();
+        this.time = 7;
+
+        var self = this;
+
+        $scope.reset = function () {
+            $scope.isSnaping = false;
+            $scope.isChoosingUsers = false;
+            $scope.isSelectingTime = false;
+            ToolsService.removeAllChildren(snapHolder);
+            self.image = null;
+            self.time = 7;
+        };
+
+        $scope.launchCamera = function () {
+            SnapService.takePicture(function (data) {
+                self.image = data;
+                ToolsService.removeAllChildren(snapHolder);
+                var img = new Image();
+                img.src = data;
+                img.className += 'send-snap-img';
+                img.onload = function () {
+                    snapHolder.appendChild(img);
+                    $scope.isSnaping = true;
+                    $scope.isGoingToChooseUsers = true;
+                    $scope.$apply();
+                };
+            });
+        };
+
+        $scope.chooseUsers = function () {
+            $scope.isGoingToChooseUsers = false;
+            $scope.isChoosingUsers = true;
+            $scope.isSelectingTime = false;
+            UserService.getUsers(self.credentials, function (response) {
+                var users = [];
+                JSON.parse(response.data.data).forEach(function (id) {
+                    users.push({
+                        id: id,
+                        isChecked: false
+                    });
+                });
+                $scope.users = users;
+            });
+        };
+
+        $scope.selectTime = function () {
+            $scope.isGoingToChooseUsers = false;
+            $scope.isChoosingUsers = false;
+            $scope.isSelectingTime = true;
+            $scope.values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            $scope.time = self.time;
+        };
+
+        $scope.setTime = function (value) {
+            self.time = value;
+        };
+
+        $scope.sendSnap = function () {
+            var destinataires = $scope.users.filter(function (object) {
+                return object.isChecked === true;
+            });
+
+            SnapService.sendSnap(self.time, self.credentials, destinataires, self.image, function (data) {
+                console.log(data);
+                $ionicPopup.alert({
+                    title: 'Well done !',
+                    template: 'Your snap has been shipped!'
+                }).then(function () {
+                    $scope.reset();
+                });
+
+            });
+        };
+    });
 }());
